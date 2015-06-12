1a3325cf09b2568a3639c29f32e0aac708f2a764
hgz6536 <hguangzu@gmail.com>
2015-06-12 21:08:05 +0800

    add mysql bakup

---
+++ mysql/myhotbak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+#     /?\__/?\              #$$$$$$$
+#   <( *   *  )>           #       &)
+#    ( @ @ )  -------?    #&&&&&&&)
+#      ~    \        $\  #        &)
+#           &\.;----/ \ #$$$$$$$$$
+# Mywblog: www.niubilety.com
+# Email: hgz@niubilety.com
+
